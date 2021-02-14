2b4d319a9ed1a3f6f85b8ad2f6b1ef6788fe24e7
aG00Dtime <77511476+aG00Dtime@users.noreply.github.com>
2021-02-14 00:22:28 -0400

    Got rid of error warning message

--- Annual_Registration_Motor_Vehicles_2000_2018.py
+++ Annual_Registration_Motor_Vehicles_2000_2018.py
@@ -1,53 +1,44 @@
-# Your python program must accomplish the following:
-# Read selected data file and output its content and output the rows
-# Output statistical summaries of the data file. You will decide on what summaries are appropriate and useful
-# Produce at least 2 graphs on some aspect of the dataset. You will decide what graphs/charts you will produce
-# Perform and output results of comparative statistical analyses of the data of any two of the years of data available in your dataset
-# Perform Any ONE other creative analysis of some aspect of the data in the dataset.
+# need to install these two libs to be able to run this
+# https://matplotlib.org/stable/index.html
+# https://pandas.pydata.org/pandas-docs/stable/getting_started/index.html
 
-# to do
-# print the rows-done
-# summaries- done,kinda?
-# graphs -done
-# compare-to do- have an idea,kinda
-# idk-?
 
-# <--- code STARTS here --->
+#  <--- code STARTS here --->
 
 # import libs needed
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # <----Csv file handling starts here---->
 # read the csv file
 csv_file = pd.read_csv(
-    "Annual_Registration_Motor_Vehicles_2000_2018.csv", skiprows=[0])  # skiprows-skips the title at the top of the csv
+    "Annual_Registration_Motor_Vehicles_2000_2018.csv", skiprows=[0])
+# replace spaces with underscores in the header
+csv_file.columns = [col.replace(
+    " ",  "_") for col in csv_file.columns]
 
-# only select the rows we want to use
+
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iloc.html
+# the name of the dataframe is vehicle_registration_df - only select the 19 rows we used
 vehicle_registration_df = csv_file.iloc[:19]
 
-# disables the warning when we change the subset of data from str to ints
-pd.options.mode.chained_assignment = None
 
-# converts the values in the df to  int
-for row in vehicle_registration_df:
-    vehicle_registration_df[row] = vehicle_registration_df[row].astype(int)
-    # print(str(vehicle_registration_df[row]))
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.loc.html
+# convert datatypes to  ints except the year column
+vehicle_registration_df.loc[:,
+                            vehicle_registration_df.columns != 'Period'].astype(float)
 
-# make the year column a str so it doesn't break the  year filter and graphs
-vehicle_registration_df.Period = vehicle_registration_df.Period.astype(str)
 
-# replace spaces in header names with underscores
-vehicle_registration_df.columns = [col.replace(
-    " ",  "_") for col in vehicle_registration_df.columns]
 # <----Csv file handling ends here---->
+
+# thousands seperator for outputs,eg. 5,000 or 20,000 etc
+pd.options.display.float_format = '{:,.0f}'.format
 
 
 def summaries():
     ''' some summaries of the data ,means,sums '''
     # summaries
-    # adds thousands seperator
-    pd.options.display.float_format = '{:,.0f}'.format
 
     # print summaries
     print("\nAVERAGE VEHICLES (BY TYPE) REGISTERED PER YEAR FROM 2000 - 2018\n")
@@ -56,11 +47,13 @@ def summaries():
     # converted to float because base values are ints
     print("\nTOTAL VEHICLES (BY TYPE) REGISTERED FROM 2000 - 2018\n ")
     print(vehicle_registration_df[['Private_Cars', 'Hire_Cars', 'Lorries', 'Buses',
-                                   'Station_Wagons', 'Vans', 'Tractors', 'Trailers', 'Motorcycles', 'Other']].sum().astype(float).to_string())
+                                   'Station_Wagons', 'Vans', 'Tractors', 'Trailers', 'Motorcycles', 'Other']].sum().to_string())
 
     print("\nTOTAL VEHICLES REGISTERED FROM 2000 - 2018\n ")
     # converted to float because base values are ints
-    print((vehicle_registration_df[['Total']].sum().astype(float).to_string()))
+    print((vehicle_registration_df[['Total']].sum().to_string()))
+    # need to do this
+    print("\nMost registered vehicle was x and the least registered was y WIP\n ")
 
 
 def graphs():
@@ -141,10 +134,10 @@ def year_view():
 
         # incorrect year check/isdecimal() returns true if only numbers are entered
         if not ask_user_for_year.isdecimal():
-            print("Enter numbers only")
+            print("\nEnter numbers only")
 
         elif 2000 > int(ask_user_for_year) or int(ask_user_for_year) > 2018:
-            print("Incorrect Year")
+            print("\nIncorrect Year")
             continue
         else:
             break
@@ -154,15 +147,6 @@ def year_view():
     # print the data
     print(custom_year.to_string(index=False))
     # ask if they want to search again
-    while True:
-        user_go_again = input(
-            "\nWould You like to enter another year? [Y]es,[N]o : ")
-        if user_go_again.lower() == 'y':
-            year_view()
-        elif user_go_again.lower() == 'n':
-            break
-        else:
-            continue
 
 
 def year_vs_year():
@@ -175,22 +159,22 @@ def year_vs_year():
         year1_ask = input("Enter the 1st year :")
         # incorrect year check/isdecimal() returns true if only numbers are entered
         if not year1_ask.isdecimal():
-            print("Enter numbers only")
+            print("\nEnter numbers only")
 
         elif 2000 > int(year1_ask) or int(year1_ask) > 2018:
-            print("Incorrect Year")
+            print("\nIncorrect Year")
             continue
         else:
             break
 
     while True:
-        year2_ask = input("Enter the 2nd year :")
+        year2_ask = input("\nEnter the 2nd year :")
         # incorrect year check/isdecimal() returns true if only numbers are entered
         if not year2_ask.isdecimal():
-            print("Enter numbers only")
+            print("\nEnter numbers only")
 
         elif 2000 > int(year2_ask) or int(year2_ask) > 2018:
-            print("Incorrect Year")
+            print("\nIncorrect Year")
             continue
         else:
             break
@@ -205,15 +189,14 @@ def year_vs_year():
     print(joined_df.set_index("Period").diff())
 
     # print(joined_df.diff(periods=-1))
+    # plot graphs of the two years
+    joined_df.plot.bar(x='Period')
+    # labels
+    plt.xlabel("Years")
+    plt.ylabel("Amount of Vehicles Registered")
 
-    # joined_df.plot.bar(x='Period')
-
-    # # labels
-    # plt.xlabel("Years")
-    # plt.ylabel("Amount of Vehicles Registered")
-
-    # # show the graph
-    # plt.show()
+    # show the graph
+    plt.show()
 
 
 def prompt():
@@ -222,7 +205,10 @@ def prompt():
         print(
             "\nTo view all the registration data from the year 2000 to 2018 Enter [1]\nTo view data by year Enter [2]\nTo view graphs of the data Enter [3]\nTo view summaries of the data Enter [4]\nTo compare two years Enter [5]\nTo exit the program Enter [0] ")
         ask_user = input('Choice : ')
-        if int(ask_user) == 1:
+
+        if not ask_user.isdecimal():
+            print("\nEnter numbers only")
+        elif int(ask_user) == 1:
             print(vehicle_registration_df.to_string(index=False))
 
         elif int(ask_user) == 2:
