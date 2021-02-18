9f10874618087e8187faa11e6fa186d2e93a873e
aG00Dtime <77511476+aG00Dtime@users.noreply.github.com>
2021-02-18 01:27:09 -0400

    Loop over values,Cleanup

--- Annual_Registration_Motor_Vehicles_2000_2018.py
+++ Annual_Registration_Motor_Vehicles_2000_2018.py
@@ -3,48 +3,79 @@
 # https://pandas.pydata.org/pandas-docs/stable/getting_started/index.html
 
 #  <--- code STARTS here --->
+
 # import libs needed
+from os import sep
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
 # <----Csv file handling starts here---->
 # read the csv file
+
 csv_file = pd.read_csv(
     "Annual_Registration_Motor_Vehicles_2000_2018.csv", skiprows=[0])
+
 # replace spaces with underscores in the header
 csv_file.columns = [col.replace(
     " ",  "_") for col in csv_file.columns]
+
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iloc.html
 # the name of the dataframe is vehicle_registration_df - only select the 19 rows we used
-vehicle_registration_df = csv_file.iloc[:19]
+
+vehicle_registration_df = csv_file.loc[:18]
+
 # data for quarterly registrations from 2014 to 2018
-vehicle_registration_df_quarterly_data = csv_file.iloc[23:53]
+vehicle_registration_df_quarterly_data = csv_file.loc[23:52]
+
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.loc.html
+
 # convert datatypes to  ints except the year column
 vehicle_registration_df.loc[:,
                             vehicle_registration_df.columns != 'Period'].astype(float)
+
 # thousands seperator for outputs,eg. 5,000 or 20,000 etc
+
 pd.options.display.float_format = '{:,.0f}'.format
+
 # <----Csv file handling ends here---->
+
+
+def sum_for():
+    avg = [vehicle_registration_df.columns !=
+           'Period' and vehicle_registration_df.columns != "Total"]
+    print(avg)
 
 
 def summaries():
     # summaries
     # https: // pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_values.html
     # print summaries
-    print("\nAVERAGE VEHICLES (BY TYPE) REGISTERED PER YEAR FROM 2000 - 2018 (Sorted from Highest to Lowest)\n")
-    print(vehicle_registration_df[['Private_Cars', 'Hire_Cars', 'Lorries', 'Buses',
-                                   'Station_Wagons', 'Vans', 'Tractors', 'Trailers', 'Motorcycles', 'Other']].mean().sort_values(ascending=False).to_string())
-    # converted to float because base values are ints
-    print("\nTOTAL VEHICLES (BY TYPE) REGISTERED FROM 2000 - 2018 (Sorted from Highest to Lowest)\n ")
-    print(vehicle_registration_df[['Private_Cars', 'Hire_Cars', 'Lorries', 'Buses',
-                                   'Station_Wagons', 'Vans', 'Tractors', 'Trailers', 'Motorcycles', 'Other']].sum().sort_values(ascending=False).to_string())
+    df = vehicle_registration_df
 
-    print("\nTOTAL VEHICLES REGISTERED FROM 2000 - 2018\n ")
-    # converted to float because base values are ints
-    print((vehicle_registration_df[['Total']].sum().to_string()))
+    # drop the columns we don't need
+    mean = df.drop(columns=['Period', 'Total'],
+                   inplace=False).mean().sort_values(ascending=False)
+    sum_ = df.drop(columns=['Period', 'Total'],
+                   inplace=False).sum().sort_values(ascending=False)
+
+    high_name, high_val = mean.head(1).to_string().split('   ', 1)
+    low_name, low_val = mean.tail(1).to_string().split('   ', 1)
+
+    print("\nAVERAGE VEHICLES (BY TYPE) REGISTERED PER YEAR FROM 2000 - 2018 (Sorted from Highest to Lowest)",
+          mean.to_string(), sep='\n')
+    print("With the Most common registered being", high_name, "with",
+          high_val, "on average \nand the least being ", low_name, "with", low_val, "on average.")
+
+    print("\nTOTAL VEHICLES (BY TYPE) REGISTERED FROM 2000 - 2018 (Sorted from Highest to Lowest)",
+          sum_.to_string(), sep='\n')
+
+    print("\nTOTAL VEHICLES REGISTERED FROM 2000 - 2018 ",
+          vehicle_registration_df[['Total']].sum().to_string(), sep='\n')
 
 
 def graphs():
+
     # graphs
     while True:
         print(
@@ -52,53 +83,35 @@ def graphs():
         user_graph_choice = input("Choice : ")
 
         if int(user_graph_choice) == 1:
-            # plot.bar plots everthing in the dataframe next to each other
-            # vehicle_registration_df.plot(x='Period')
 
-            # private cars
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Private_Cars)
-            # lorries
+            # skip period column
+            graph_data = vehicle_registration_df.columns[1:]
 
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Lorries)
-            # vans
+            # loop through columns to create a graph
+            for col in graph_data:
+                plt.plot(
+                    vehicle_registration_df.Period, vehicle_registration_df[col])
 
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Vans)
-            # hire cars
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Hire_Cars)
-            # buses
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Buses)
-            # Station Wagons
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Station_Wagons)
-            # tractors
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Tractors)
-
-            # trailers
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Trailers)
-            # motorcycles
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Motorcycles)
-
-            # other vehicles
-            plt.plot(vehicle_registration_df.Period,
-                     vehicle_registration_df.Other)
-
-            # labels
+            # x & y labels
             plt.xlabel("Years")
             plt.ylabel("Amount of Vehicles Registered")
-            plt.legend(['Private cars', 'Lorries',
-                        'Vans', 'Hire Cars', "Buses", 'Station Wagons', 'Tractors', 'Trailers', 'Motor Cycles', 'Other vehicles'])
+
+            # create legend names
+            col_names = []
+
+            for col in graph_data:
+                col_names.append(col)
+
+            # remove total from legend
+            del col_names[-1]
+
+            plt.legend(col_names)
+
             # show the graph
             plt.show()
 
         elif int(user_graph_choice) == 2:
+
             # total amount of vehicles registered
             plt.bar(vehicle_registration_df.Period,
                     vehicle_registration_df.Total)
@@ -110,11 +123,12 @@ def graphs():
             break
 
         else:
-            print("invalid choice")
+            print("Invalid choice")
             continue
 
 
 def year_view():
+
     # outputs a single year's data
     while True:
         ask_user_for_year = (input("\nPlease enter a year :"))
@@ -128,6 +142,7 @@ def year_view():
             continue
         else:
             break
+
         # find data for the year entered
     custom_year = vehicle_registration_df[vehicle_registration_df.Period ==
                                           ask_user_for_year]
@@ -136,6 +151,7 @@ def year_view():
 
 
 def year_vs_year():
+
     # shows the difference between two years and produces a graph that shows the two years side by side
     df = vehicle_registration_df
     while True:
@@ -150,6 +166,7 @@ def year_vs_year():
             break
 
     while True:
+
         year2_ask = input("\nEnter the 2nd year :")
         # incorrect year check/isdecimal() returns true if only numbers are entered
         if not year2_ask.isdecimal():
@@ -168,24 +185,33 @@ def year_vs_year():
     # combine two years into 1 df
     joined_df = [year1, year2]
     joined_df = pd.concat(joined_df)
+
     # copy values here for the graph
     graph_values = joined_df
+
     # get the difference of the two years , .abs (absolute value) to get the absolute values and not negatives
     difference = joined_df.set_index("Period").diff().abs()
+
     # only show the difference in a single row
     difference = difference.iloc[1:2]
+
     # combime the row with the difference to the df with the years
     joined_df = [joined_df, difference]
     joined_df = pd.concat(joined_df)
+
     # because the index was set to 'period' ,the column goes blank when merged,so we can replace that NaN value with 'Difference' :),Perfect
     joined_df = joined_df.fillna('Difference')
+
     # print the data
     print('\n', joined_df.to_string(index=False))
+
     # plot graphs of the two years
     graph_values.plot.bar(x='Period')
+
     # labels
     plt.xlabel("Years")
     plt.ylabel("Amount of Vehicles Registered")
+
     # show the graph
     plt.show()
 
